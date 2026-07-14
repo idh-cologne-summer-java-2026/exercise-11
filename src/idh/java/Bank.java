27d0562d7de419b91d897d7a78efb7d66255a5bf
amalorod <alicmalorodow@gmail.com>
2026-07-14 18:23:39 +0200

    Lösung Aufgabe 1

--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -1,17 +1,20 @@
 package idh.java;
 
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Random;
 
 public class Bank implements Iterable<Account> {
 	Account[] accounts = new Account[5];
+	HashMap<Integer, Account> map = new HashMap<Integer, Account>();
 
 	public Bank() {
 		// create accounts with varying balances
 		Random random = new Random();
 		for (int i = 0; i < accounts.length; i++) {
 			accounts[i] = new Account(i, random.nextInt(1000));
+			 map.put(accounts[i].getId(), accounts[i]);
 		}
 	}
 	
@@ -21,8 +24,8 @@ public class Bank implements Iterable<Account> {
 	}
 	
 	public Account getAccount(int number) {
-		// TODO: Implement
-		return null;
+		
+		return map.get(number);
 	}
 
 }
