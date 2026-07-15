006a3f6f9032d4458adbb8d7771e0fca76f1b1c5
serradani <strawberryanima@proton.me>
2026-07-15 05:07:43 +0200

    assignment donezo

--- src/idh/java/Bank.java
+++ src/idh/java/Bank.java
@@ -3,26 +3,28 @@ package idh.java;
 
 import java.util.Iterator;
 import java.util.Random;
+import java.util.HashMap;
 
 public class Bank implements Iterable<Account> {
-	Account[] accounts = new Account[5];
+	//to maintain the fixed size of 5 even though we now use a dynamic data structure
+	int placeHolderSize = 5;
+	Account[] accts = new Account[placeHolderSize];
+	HashMap<String, Account> accounts = new HashMap<>();
 
 	public Bank() {
 		// create accounts with varying balances
 		Random random = new Random();
-		for (int i = 0; i < accounts.length; i++) {
-			accounts[i] = new Account(i, random.nextInt(1000));
+		for (int i = 0; i < placeHolderSize; i++) {
+			accounts.put(Integer.toString(i), new Account(Integer.toString(i), random.nextInt(1000)));
 		}
 	}
 	
 	@Override
 	public Iterator<Account> iterator() {
-		return new AccountIterator(accounts);
+		return new AccountIterator(accts);
 	}
 	
-	public Account getAccount(int number) {
-		// TODO: Implement
-		return null;
+	public Account getAccount(String number) {
+		return accounts.get(number);
 	}
-
 }
